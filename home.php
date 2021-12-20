d1812aefed9c5b0e8f253b2fb82c0083ebac7db3
romain <romainmichel@ynov.com>
2021-12-20 19:15:03 +0100

    details page

--- home.php
+++ home.php
@@ -7,34 +7,68 @@
  }  
  $mysqli = new mysqli("localhost", "admin", "PassWord!", "php_exam_db");  
 
- $result = $mysqli->query("SELECT * FROM `events` ORDER BY PostDate DESC; ");
- ?>  
- <!DOCTYPE html>  
- <html>  
-     <head>  
-          <title>Accueil</title> 
-          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-          <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-          <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-          <link rel="stylesheet" href="css/home.css" />
-          <link rel="stylesheet" href="css/navbar.css" />
-      </head>  
-      <body>  
-      <?php include 'navbar.php'?>
-           <br /><br />  
-           <div class="container" style="width:500px;">  
-                <h3>Bienvenue sur cette page d'acceuil en travaux</h3>  
+ $result = $mysqli->query("SELECT Title, Content, StartDate, Localisation, events.Id, users.Username FROM `events` INNER JOIN  users ON events.UserId = users.Id ORDER BY PostDate DESC; ");
+ 
+ while($row = $result->fetch_array())
+ {
+ $rows[] = $row;
+ }
 
-                <?php  
-                echo '<h1>Bienvenue '.$_SESSION["username"].'</h1>';  
-                echo '<h1>ID : '.$_SESSION["userId"].'</h1>';  
-                echo '<label><a href="logout.php">Logout</a></label>';  
-                ?>  
-                <?php while ($row = mysql_fetch_assoc($result)): ?>
-                    <p><?php echo $row['titre'];?><p>
-                <?php endwhile ?>
-           </div>  
-      </body>  
- </html>  
+?>  
+<!DOCTYPE html>  
+<html>  
+     <head>  
+         <title>Accueil</title> 
+         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+         <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+         <link rel="stylesheet" href="css/home.css" />
+         <link rel="stylesheet" href="css/navbar.css" />
+     </head>  
+     <body>  
+     <?php include 'navbar.php'?>
+
+          <div class="intro">  
+               <?php  
+               echo '<h1>Bienvenue '.$_SESSION["username"].', tu peux retrouver tous les évènements qui ont étés postés sur ce site ci-dessous</h1>';  
+               ?> 
+               <h3>Ce site est le repère des alcooliques du coin, tu veux pécho? frôler la sieste éthylique ? ce site est fait pour toi</h3>
+          </div>  
+
+          <div class="eventList">
+               <?php  
+               if(count($rows) > 0 ){
+
+                for($i = count($rows); $i > 0 ; $i){
+                    $finalRows[] = array_slice($rows,0,3);
+                    $i = $i-3;
+                }
+
+               foreach($finalRows as $rows){
+                    echo '<div class="eventRow">';
+                    foreach($rows as $row){
+                         echo '<div class="event">';
+                         echo '<div class="eventHead">';
+                         echo '<h2><b>'.$row['Title'].'</b><i> par '.$row['Username'].'</i></h2>';
+                         echo '<button style="width:10vh; margin: 1vh" class="detailsBtn"><a style="color:black"href="details.php?id='.$row['Id'].'">Détails</a> </button>';
+                         echo '</div>';
+                         echo '<div class="content"><p>'.$row['Content'].'<p></div>';
+                         echo '<p><i> aura lieu le : '.$row['StartDate'].' à '.$row['Localisation'].'</i></p>';
+
+                         echo '</div>';
+                    }
+                    echo '</div>';
+               }
+               }else{
+                    echo '<div class="eventRow">';
+                    echo '<div class="event">';
+                         echo '<h2>il n\'y a pas d\'event, quel site de merde</h2>';
+                         echo '</div>';
+                    echo '</div>';
+               }
+               ?> 
+          </div>
+     </body>  
+</html>  
